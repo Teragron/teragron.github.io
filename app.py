8b23bb47791a6e6194f970a858f6d56495299428
Can Günen <teragron@github.com>
2023-05-05 15:55:15 +0200

    now we can also upload images to blogs

--- app.py
+++ app.py
@@ -1,37 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  5 11:30:31 2023
-
-@author: ahmet
-"""
-
 import datetime
+import gradio as gr
+import cv2
+import uuid
 
-# Get the current date and time
-now = datetime.datetime.now()
+def generate_markdown_post(title: str, content: str, image: gr.Image):
+    # Get the current date and time
+    now = datetime.datetime.now()
 
-# Format the date and time in the required format
-date_string = now.strftime("%Y-%m-%d %H:%M:%S %z")
-subdate_string = now.strftime("%Y-%m-%d")
+    # Format the date and time in the required format
+    date_string = now.strftime("%Y-%m-%d %H:%M:%S %z")
+    subdate_string = now.strftime("%Y-%m-%d")
 
+    # Generate a random unique ID
+    unique_id = str(uuid.uuid4().hex)[:6]
 
-# Get the blog post title from the user
-title = input("Enter the blog post title: ")
+    # Format the filename with subdate and unique ID
+    filename = f"_posts/{subdate_string}-{unique_id}.markdown"
 
-# Format the title and date in the filename
-filename = f"teragron.github.io/_posts/{subdate_string}-{title.replace(' ', '-').lower()}.markdown"
-
-content = input("Please enter content: ")
-# Define the Markdown content with the updated title and date
-markdown_content = f"""---
+    # Define the Markdown content with the updated title and date
+    markdown_content = f"""---
 layout: post
-title:  "{title}"
-date:   {date_string}+200
+title: "{title}"
+date: {date_string}+200
 categories: jekyll update
 ---
+<img src="/blog_assets/{unique_id}.jpg">
+
 {content}
 """
 
-# Write the Markdown content to the file with the formatted filename
-with open(filename, "w") as f:
-    f.write(markdown_content)
+    # Save the uploaded image to the file system
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    cv2.imwrite(f"blog_assets/{unique_id}.jpg",image)
+
+    # Write the Markdown content to the file with the formatted filename
+    with open(filename, "w") as f:
+        f.write(markdown_content)
+
+    return f"Markdown post created at {filename}"
+
+title = "Markdown Blog Post Generator"
+description = "This app generates a Markdown blog post with the given title, content, and image."
+inputs = [
+    gr.Textbox(label="Enter the blog post title:"),
+    gr.Textbox(label="Enter the content:"),
+    gr.Image(label="Upload an image")
+]
+outputs = gr.Textbox()
+gr.Interface(fn=generate_markdown_post, inputs=inputs, outputs=outputs, title=title, description=description).launch()
