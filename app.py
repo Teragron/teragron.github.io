40320cf62cdf5e697d14f94d706548b169b89f1e
Can Günen <teragron@github.com>
2023-05-06 02:06:38 +0200

    New blog post eb2d92

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def commit_and_push(file_path: str):
     subprocess.call(['git', 'add', "."])
 
     # Commit the changes
-    subprocess.call(['git', 'commit', '-m', 'New blog post'])
+    subprocess.call(['git', 'commit', '-m', f'New blog post {unique_id}'])
 
     # Push the changes to the remote repository
     subprocess.call(['git', 'push'])
@@ -18,7 +18,7 @@ def commit_and_push(file_path: str):
     return "Markdown post committed and pushed successfully!"
 
 
-def generate_markdown_post(title: str, content: str, image=None):
+def generate_markdown_post(title: str, content: str, image: gr.Image):
     # Get the current date and time
     now = datetime.datetime.now()
 
@@ -27,16 +27,18 @@ def generate_markdown_post(title: str, content: str, image=None):
     subdate_string = now.strftime("%Y-%m-%d")
 
     # Generate a random unique ID
+    global unique_id
     unique_id = str(uuid.uuid4().hex)[:6]
+    
 
     # Format the filename with subdate and unique ID
     filename = f"_posts/{subdate_string}-{unique_id}.markdown"
     
-    if image:
+    try:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         cv2.imwrite(f"blog_assets/{unique_id}.jpg",image)
         src = '<img src="/blog_assets/{unique_id}.jpg">'
-    else:
+    except:
         src = ""
 
     # Define the Markdown content with the updated title and date
