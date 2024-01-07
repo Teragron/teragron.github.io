4095ff85563057b2873259a57162d3f9b253c732
Teragron <54573938+Teragron@users.noreply.github.com>
2024-01-07 23:09:08 +0100

    Update app.bat

--- app.bat
+++ app.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\Users\ahmet\anaconda3\envs\valley\python.exe" "C:\Users\ahmet\Desktop\teragron.github.io/app.py"
+"C:\Users\give_custom\python.exe" "C:\Users\give_custom\teragron.github.io/app.py"
 start http://127.0.0.1:7861
 pause
